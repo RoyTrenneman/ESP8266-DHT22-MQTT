0525a53cd17c803e4be2e6f93a9720b20d3d8968
RoyTrenneman <ar@debianAR.home>
2017-04-22 17:34:01 +0200

    remove unused function

--- src/Autoconnect.ino
+++ src/Autoconnect.ino
@@ -59,26 +59,6 @@ void Get_voltage() {
  voltage = (4.3/1023)*adc;
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
-}
-
 void reconnect() {
   // Loop until we're reconnected
  try_again = 0 ;
@@ -196,9 +176,9 @@ void setup() {
  
  //Start a voltage measure
   Get_voltage();
-  
+ 
+ //Start MQTT client 
   client.setServer(Portal.mqtt_server, 1883);
-  client.setCallback(callback);
   Serial.println(Portal.mqtt_server) ;
  
  //if you get here you have connected to the WiFi
